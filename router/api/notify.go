98672b126dd0fe04efb1172f0cc607d130cf4b38
李寻欢 <lxh@cxh.cn>
2023-09-29 18:55:15 +0800

    :sparkles: 新增BBK扫码回调接口

--- router/api/notify.go
+++ router/api/notify.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"pixiu-panel/api/api/bbk"
 	"pixiu-panel/api/api/ql"
 	"pixiu-panel/api/api/qq"
 	"pixiu-panel/api/api/wechat"
@@ -14,4 +15,5 @@ func notify(g *gin.RouterGroup) {
 	g.POST("/message", ql.MessageNotify) // 消息通知接口(模拟实现 gotify)
 	g.POST("/wechat", wechat.Notify)     // 微信消息回调
 	g.POST("/qq", qq.Notify)             // QQ消息回调
+	g.POST("/bbk", bbk.Notify)           // BBK消息回调
 }
